ea05fb064a2ee71d3acc1fce4e3075ac3474ac80
MiquelRR <miquel.poli@gmail.com>
2024-06-19 20:42:21 +0200

    solving

---
+++ odp/src/main/java/com/model/clean_types_models.sql
@@ -0,0 +1,3 @@
+DELETE FROM odplanDDBB.taskDependency where idtaskInType<1000;
+DELETE FROM odplanDDBB.liveTask where idLiveTask < 1000;
+DELETE FROM odplanDDBB.productType WHERE idproductType<1000;
